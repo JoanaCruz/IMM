450121cf71b860139e16f8e097387606ec51a911
JoanaCruz <joanacruz1@gmail.com>
2012-09-04 06:50:42 -0700

    commented input, with erased

--- trim.py
+++ trim.py
@@ -1,13 +1,14 @@
 from sys import argv
 import gzip
 
+# nome script, fast_file (sem .fq.gz), nº bases a eliminar
 script, fast_file, delete =argv
 
 fastq_file=fast_file + ".fq.gz"
 delete=int(delete)
 
-with gzip.open(fastq_file, mode='rb') as inf:
-	out_file = gzip.open(fast_file + "_trimmed.gz", mode='wb',compresslevel=9)
+inf=gzip.open(fastq_file, mode='rb') as inf:
+out_file = gzip.open(fast_file + "_trimmed.gz", mode='wb',compresslevel=9)
 	for (i, line) in enumerate(inf):
 		if i % 2 != 0:
 			out_file.write(line[delete:])
