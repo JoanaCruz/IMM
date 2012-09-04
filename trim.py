b7b9a412d391c068e33f3143ac87bf2f5839d9e8
JoanaCruz <joanacruz1@gmail.com>
2012-09-04 07:21:46 -0700

    input: script name, fast_file (without .fq.gz), nº bp to trim

--- trim.py
+++ trim.py
@@ -1,7 +1,6 @@
 from sys import argv
 import gzip
 
-# nome script, fast_file (sem .fq.gz), nº bases a eliminar
 script, fast_file, delete =argv
 
 fastq_file=fast_file + ".fq.gz"
@@ -9,16 +8,16 @@ delete=int(delete)
 
 inf=gzip.open(fastq_file, mode='rb') as inf:
 out_file = gzip.open(fast_file + "_trimmed.gz", mode='wb',compresslevel=9)
-	for (i, line) in enumerate(inf):
-		if i % 2 != 0:
-			out_file.write(line[delete:])
-			if i in range (0,10):
-				print line[delete:]
-				print line
-		else:
-			out_file.write(line)
-			if i in range (0,10):
-				print line
+for (i, line) in enumerate(inf):
+	if i % 2 != 0:
+		out_file.write(line[delete:])
+		if i in range (0,10):
+			print line[delete:]
+			print line
+	else:
+		out_file.write(line)
+		if i in range (0,10):
+			print line
 
 inf.close()
 out_file.close()
