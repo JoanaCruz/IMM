36017ea273a65887aee44188720329431a147717
Joana <joana@ubuntu.(none)>
2012-10-04 09:03:51 -0700

    Upgrades added to paired end script.

--- paired_end.py
+++ paired_end.py
@@ -1,10 +1,10 @@
 from sys import argv
 import gzip
 
-script, directory, sam_file =argv
+script, in_directory, sam_file, out_directory, =argv
 
-inf=gzip.open(directory + sam_file +".sam.gz", mode='rb')
-out_file = gzip.open(sam_file + "_paired.sam.gz", mode='wb',compresslevel=9)
+inf=gzip.open(directory + sam_file + "_sorted.sam.gz", mode='rb')
+out_file = gzip.open(out_directory + sam_file + "_paired.sam.gz", mode='wb',compresslevel=9)
 j=0
 for (i,line) in enumerate(inf):
 	if line[0] == "@":
@@ -15,7 +15,6 @@ for (i,line) in enumerate(inf):
 			l = line.split()[2]
 			name = line
 		else:
-			print l
 			if l == line.split()[2]:
 				out_file.write(name)
 				out_file.write(line)
