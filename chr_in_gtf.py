8fc02748f19c4b093bcaeb6b5cc8bdbe5b17b805
Joana Cruz <joanacruz1@gmail.com>
2012-10-12 15:59:06 -0700

    Error corrections.

--- chr_in_gtf.py
+++ chr_in_gtf.py
@@ -4,14 +4,15 @@ import gzip
 script, in_directory, input_file, out_directory =argv
 
 inf = gzip.open(in_directory + input_file + ".gtf.gz", mode='rb')
-out_file = open(out_directory + input_file + "_chr.gtf.gz", 'w') 
+out_file = open(out_directory + input_file + "_chr.gtf", 'a') 
 
 for (i,line) in enumerate(inf):
 	string=line.split()
 	ins = string[13] 
 	string[13] = ins[0]+"chr"+ins[1:]
 	string=" ".join(string)
-	out_file.write(string)
+	print string
+	out_file.write(string +"\n")
 
 inf.close()
 out_file.close()
