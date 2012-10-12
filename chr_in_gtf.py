ffcefc306b6ce592732753a060e48493bed2bcfd
Joana <joana@ubuntu.(none)>
2012-10-12 13:30:44 -0700

    chr_in_gtf script updated

--- chr_in_gtf.py
+++ chr_in_gtf.py
@@ -4,12 +4,14 @@ import gzip
 script, in_directory, input_file, out_directory =argv
 
 inf = gzip.open(in_directory + input_file + ".gtf.gz", mode='rb')
-out_file = gzip.open(out_directory + input_file + "_chr.gtf.gz", mode='wb',compresslevel=9) 
+out_file = open(out_directory + input_file + "_chr.gtf.gz", 'w') 
 
 for (i,line) in enumerate(inf):
 	string=line.split()
 	ins = string[13] 
 	string[13] = ins[0]+"chr"+ins[1:]
-	
+	string=" ".join(string)
+	out_file.write(string)
+
 inf.close()
 out_file.close()
