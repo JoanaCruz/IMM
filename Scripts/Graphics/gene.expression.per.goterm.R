41a0a196e8ea9debb006bd33d11bd69a2e0d9930
Joana Cruz <joanacruz1@gmail.com>
2013-02-04 20:13:44 -0800

    round function was missing

--- Scripts/Graphics/gene.expression.per.goterm.R
+++ Scripts/Graphics/gene.expression.per.goterm.R
@@ -1,6 +1,6 @@
 ## Load data
 args <- commandArgs(TRUE)
-#args <- c("LM1_HeLa_all_timep_final.txt", "exp/", "LM1", c("GOsummary_BP_20.csv"))
+#args <- c("LM1_HeLa_all_timep_final.txt", "exp/", "LM1", c("GOsummary_BP_120.csv"))
 count_table = read.table(file = args[1], header=TRUE, row.names=1)
 
 
@@ -46,13 +46,14 @@ geneExpressionPlot <- function ( gotable, count_table, goname){
 	genes=gotable$genes_RefSeq
 	goterms=as.list(gotable[1])
 	if ( length(genes) > 30 ) { sizeGenes=30 } else { sizeGenes=length(genes) }
-	for ( i in 1:sizeGenes){ 
+	for ( i in 1:sizeGenes){
 		splitData=strsplit(as.character(genes[i]),", ")
 		length_splitdata=length(splitData[[1]])
 		sum=length_splitdata%%2
 		gosplit=strsplit(as.character(goname),"_")
 		go_ont=gosplit[[1]][2]
 		go_timep=strsplit(as.character(gosplit[[1]][3]),"[.]")[[1]][1]
+		if(length_splitdata<10){
 		png(filename= sprintf("%s%s/%s_%s_%s_%s.png", args[2], args[3], args[3], go_ont, go_timep, goterms[[1]][i])) 
 		if (length_splitdata==2){
 			par(mfrow=c(2,1), oma = c(1.5,1.5,0,0), mar = c(2.5,2.5,1.5,0.5))
@@ -66,8 +67,44 @@ geneExpressionPlot <- function ( gotable, count_table, goname){
 		mtext("Time-points [min]", side=1, outer=TRUE)
 		mtext("Nb counts normalized", side=2, outer=TRUE)
 		dev.off()
+		}
+		if(length_splitdata>10){
+		length_splitdata_var=length_splitdata/10
+		pos=1
+		while (length_splitdata_var > 0){
+			if(length_splitdata_var > 1){
+				png(filename= sprintf("%s%s/%s_%s_%s_%s_%s.png", args[2], args[3], args[3], go_ont, go_timep, goterms[[1]][i], ceiling(length_splitdata_var)))
+				par(mfrow=c(5,2), oma = c(1.5,1.5,0,0), mar = c(2.5,2.5,1.5,0.5))
+				for( j in pos:(pos+9)){
+					plotExpression(splitData[[1]][j], count_table)
+					print(j)
+				}
+				mtext("Time-points [min]", side=1, outer=TRUE)
+				mtext("Nb counts normalized", side=2, outer=TRUE)
+				dev.off()
+				pos=pos+9
+				print(pos)
+				length_splitdata_var=length_splitdata_var-1
+			}
+			else{
+				sum=length_splitdata_var%%2
+				png(filename= sprintf("%s%s/%s_%s_%s_%s_%s.png", args[2], args[3], args[3], go_ont, go_timep, goterms[[1]][i], ceiling(length_splitdata_var)))
+				#print((length_splitdata_var*10+sum)/2)
+				par(mfrow=c((length_splitdata_var*10+sum)/2,2), oma = c(1.5,1.5,0,0), mar = c(2.5,2.5,1.5,0.5))
+				for( j in pos:length_splitdata){
+				plotExpression(splitData[[1]][j], count_table)
+				}
+				print(pos)
+				mtext("Time-points [min]", side=1, outer=TRUE)
+				mtext("Nb counts normalized", side=2, outer=TRUE)
+				dev.off()
+				length_splitdata_var=0
+			}
+		}
+	}
 	}
 }
+geneExpressionPlot(gotable, normalized, "GOsummary_BP_120.csv"
 
 for( i in 4:length(args)){
 	gotable = read.csv(file = args[i], header=TRUE, row.names=1)
