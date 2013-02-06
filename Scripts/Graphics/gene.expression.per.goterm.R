0fc4f4e2e5729fcd7f472f04416914d5a7e46812
Joana Cruz <joanacruz1@gmail.com>
2013-02-05 21:26:46 -0800

    plot with 3 columns

--- Scripts/Graphics/gene.expression.per.goterm.R
+++ Scripts/Graphics/gene.expression.per.goterm.R
@@ -50,18 +50,18 @@ geneExpressionPlot <- function ( gotable, count_table, goname){
 		splitData=strsplit(as.character(genes[i]),", ")
 		#number os genes to goterm i
 		length_splitdata=length(splitData[[1]])
-		sum=length_splitdata%%2
+		#sum=length_splitdata%%2
 		gosplit=strsplit(as.character(goname),"_")
 		go_ont=gosplit[[1]][2]
 		go_timep=strsplit(as.character(gosplit[[1]][3]),"[.]")[[1]][1]
 		# if number of genes is minor than 10 we only need one plot
-		if(length_splitdata<10){
+		if(length_splitdata<15){
 			png(filename= sprintf("%s%s/%s_%s_%s_%s.png", args[2], args[3], args[3], go_ont, go_timep, goterms[[1]][i])) 
 			if (length_splitdata==2){
 				par(mfrow=c(2,1), oma = c(1.5,1.5,0,0), mar = c(2.5,2.5,1.5,0.5))
 			}
 			else if (length_splitdata>1){
-				par(mfrow=c((length_splitdata+sum)/2,2), oma = c(1.5,1.5,0,0), mar = c(2.5,2.5,1.5,0.5))
+				par(mfrow=c(ceiling((length_splitdata)/3),3), oma = c(1.5,1.5,0,0), mar = c(2.5,2.5,1.5,0.5))
 			}
 			for( j in 1:length_splitdata){
 				plotExpression(splitData[[1]][j], count_table)
@@ -72,31 +72,31 @@ geneExpressionPlot <- function ( gotable, count_table, goname){
 		}
 		
 		# if the numebr of genes is greater than 10 it is needed to do more plots (each plot has 10 entries) 
-		else if(length_splitdata>10){
-		length_splitdata_var=length_splitdata/10
+		else if(length_splitdata>15){
+		length_splitdata_var=length_splitdata/15
 		pos=1
 		while (length_splitdata_var != 0){
 			if(length_splitdata_var > 1){
 				png(filename= sprintf("%s%s/%s_%s_%s_%s_%s.png", args[2], args[3], args[3], go_ont, go_timep, goterms[[1]][i], ceiling(length_splitdata_var)))
-				par(mfrow=c(5,2), oma = c(1.5,1.5,0,0), mar = c(2.5,2.5,1.5,0.5))
+				par(mfrow=c(5,3), oma = c(1.5,1.5,0,0), mar = c(2.5,2.5,1.5,0.5))
 				
-				for( j in pos:(pos+9)){
+				for( j in pos:(pos+14)){
 					plotExpression(splitData[[1]][j], count_table)
 				}
 				
 				mtext("Time-points [min]", side=1, outer=TRUE)
 				mtext("Nb counts normalized", side=2, outer=TRUE)
 				dev.off()
-				pos=pos+10
+				pos=pos+15
 				length_splitdata_var=length_splitdata_var-1
 			}
 			else{
 				png(filename= sprintf("%s%s/%s_%s_%s_%s_%s.png", args[2], args[3], args[3], go_ont, go_timep, goterms[[1]][i], ceiling(length_splitdata_var)))
-				if ((length_splitdata_var*10)=="2"){	
+				if ((length_splitdata_var*15)=="2"){	
 					par(mfrow=c(2,1), oma = c(1.5,1.5,0,0), mar = c(2.5,2.5,1.5,0.5))
 				}
-				else if ((length_splitdata_var*10)>"1"){
-					par(mfrow=c(round((length_splitdata_var*10+sum)/2),2), oma = c(1.5,1.5,0,0), mar = c(2.5,2.5,1.5,0.5))
+				else if ((length_splitdata_var*15)>"1"){
+					par(mfrow=c(ceiling((length_splitdata_var*15)/3),3), oma = c(1.5,1.5,0,0), mar = c(2.5,2.5,1.5,0.5))
 				}	
 				for( j in pos:length_splitdata){
 					plotExpression(splitData[[1]][j], count_table)
