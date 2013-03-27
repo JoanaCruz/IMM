c311ed306c7b9256ff5b1530501d6f700cff781f
Joana Cruz <joanacruz1@gmail.com>
2013-03-27 11:40:10 -0700

    Script to correlate networks with gene expression profile updated

--- Scripts/correlate.net.py
+++ Scripts/correlate.net.py
@@ -1,98 +1,119 @@
 from sys import argv
 import linecache
+import glob
+import os
 
-
-#extract go terms related with gene from NCBI file
-def extract_go(input_file_data, nb_line):
-	i=nb_line+3
-	bol=True
-	go_data=[]
-	while bol:
-		go_line=linecache.getline(input_file_data, i)
-		go_line_find=go_line.find("GO:")
-		if go_line_find!=-1:
-			go_data.append(go_line[go_line_find:go_line_find+10])
-		else:
-			bol=False
-		i=i+1
-	return go_data,i
 
 # extract mrna data related with gene from NCBI file
-def extract_mrna(input_file_data, nb_line):
-	i=nb_line+1
-	bol=True
+def extract_mrna(input_file_data):
 	mrna_data=[]
-	while bol:
-		mrna_line=linecache.getline(input_file_data, i)
-		if mrna_line[0:4]=="\tNM_":
-			line_with_mrna=mrna_line.split()[0].split(".")[0]
-			mrna_data.append(line_with_mrna)
-		else:
-			bol=False
-		i=i+1
-	return mrna_data,i
+	source=[]
+	gene_name=[]
+	with open(input_file_data) as read_data_file:
+		for (i, line) in enumerate(read_data_file):
+			if i==0:
+				source.append(line)
+			if line[0:10]=="<pre><pre>":
+				gene_name.append(line[10:-1])
+			if line[0:4]=="\tNM_":
+				mrna_data.append(line.split()[0].split(".")[0])
+	return mrna_data, source, gene_name
 
-# mRNA and goterms associated with the gene being analyzed (summary downloaded from NCBI site)
-def get_data(input_file_data, read_data_file):
-	#initialize variables
-	parsing_go=False
-	parsing_mrna=False
-	go_data_MF, go_data_BP, go_data_CC, mrna_data = [[]]*4
 
-	for (i, line) in enumerate(read_data_file):
-		if line=="Gene Ontology Provided by GOA (www.ebi.ac.uk/GOA)\n":
-			parsing_go=True
-		if line=="GENERAL PROTEIN INFORMATION\n":
-			parsing_go=False	
-		if line==" mRNA and Protein(s)\n":
-			parsing_mrna=True
-		if line=="RELATED SEQUENCES\n":
-			parsing_mrna=False
-		if parsing_go:
-			if len(line.split())==4 and line.split()[0]=="Function":
-				go_data_MF, new_i = extract_go(input_file_data, i)
-				i=new_i
-			if len(line.split())==4 and line.split()[0]=="Processs":
-				go_data_BP, new_i = extract_go(input_file_data, i)
-				i=new_i
-			if len(line.split())==4 and line.split()[0]=="Component":
-				go_data_CC, new_i = extract_go(input_file_data, i)
-				i=new_i
-		if parsing_mrna:
-			mrna_data, new_i = extract_mrna(input_file_data, i)
-			i=new_i
-	return go_data_MF,go_data_BP, go_data_CC, mrna_data
+# write html page with gene expression profile (taking into account mrna data extracted from NCBI file)
+def write_plot_html(gene_name, mrna_data, source, plot_dir, out_directory, rownum):
+	#create folder for plot html
+	if not os.path.exists(out_directory + "html_plots/"):
+		os.makedirs(out_directory + "html_plots/")
+	with open(out_directory + "html_plots/column" + str(rownum) + ".html", "w+") as html_plot:
+		html_plot.write('<html><head><title>NETWORK ANALYSIS</title></head><body><font size="5">Network of immune response - 27th March 2013</font>\n')
+		html_plot.write('<br><br><font size="4">Gene expression profile</font>')
+		
+		html_plot.write('<p align="right"> <a href="../network_index.html"> Previous page </a> </p>')	
+		
+		html_plot.write('<table rules="all" CELLSPACING="0" COLS="5" BORDER="1"><tr>' + "\n" )
+		
+		header=["Gene name", "RefSeq", "NCBI acession"]
+		for head in header:
+			html_plot.write('<th>' + head + '</th>' + "\n" ) # write header columns
+		html_plot.write('</tr>'+ "\n") # write </tr> tag
+		
+		html_plot.write('<tr class="color1">' + "\n")
+		column=[gene_name, mrna_data, source]
+		colnum=1
+		for i in column:
+			if i==source:
+				html_plot.write('<td class="column_' + str(colnum) + '">' + '<p><a href=' + str(source[0]) +'>GO to NCBI site</a> </p></td>\n')
+			elif len(mrna_data) > 1:
+				html_plot.write('<td class="column_' + str(colnum) + '">')
+				for j in range(len(mrna_data)):
+					html_plot.write(str(i[j]) + "  ")	
+				html_plot.write('</td>\n')
+			else:
+				html_plot.write('<td class="column_' + str(colnum) + '">' + str(i[0]) + '</td>'+ "\n")
 			
-# search data in GOStats files
-def find_terms(input_file_data, read_data_file, tp):
-	go_data_MF,go_data_BP, go_data_CC, mrna_data=get_data(input_file_data, read_data_file)
-	for ont in ["MF","BP","CC"]:
-		print(ont)
-		read_go_file=open(go_directory+"/GOsummary_"+ont+"_"+tp+".csv")
-		search_result=open(out_directory+"network_seach_"+tp+"_"+ont+".txt", "w")
-		for (j,line) in enumerate(read_go_file):
-			go_line=line.split(",")
-			go_term=go_line[1].strip('"')
-			mrna=go_line[7]
-			go_data_ont=eval("go_data_"+ont)
-			found=0
-			for i in range(0,len(go_data_ont)):
-				if j==1 and i==0:
-					print(go_term==go_data_ont[i])
-				if go_term==go_data_ont[i]:
-					search_result.write(line)
-					found=found+1
-	read_go_file.close()
-	search_result.close()
-	return found
+		html_plot.write('</tr></table>')
 
-script, input_file_data, input_go_file, out_directory =argv
+		i=0
+		for i in range(len(mrna_data)):
+			html_plot.write('<br><table><tr><td><img src='+plot_dir+mrna_data[i]+".png"+ '></a></td></tr></table></body></html>')
 
-read_data_file=open(input_file_data)
-go_directory=input_go_file.split("/")[0]
-tp=input_go_file.split("_")[2].split(".")[0]
 
-# Do table with terms found
-find_terms(input_file_data, read_data_file, tp)
+def write_html(mrna_data, source, gene_name, rownum, out_directory, plot_dir):
+	with open(out_directory+"network_index.html","w+") as index:
+		print(mrna_data)
+		if rownum == 0:
+			index.write('<html><head><title>NETWORK ANALYSIS</title></head><body><font size="5">Network of immune response - 27th March 2013</font></body></html><br>\n')
+			index.write('<br><table rules="all" CELLSPACING="0" COLS="5" BORDER="1">' + "\n" )
+			index.write('<tr>'+ "\n") # write <tr> tag
+			header=["Gene name", "RefSeq", "NCBI acession", "Gene expression profile"]
+			for head in header:
+				index.write('<th>' + head + '</th>' + "\n" ) # write header columns
+			index.write('</tr>'+ "\n") # write </tr> tag
+		
+		if rownum % 2 == 0:
+			index.write('<tr class="color1">' + "\n")
+		else:
+			index.write('<tr class="color2">' + "\n")
+		column=[ gene_name, mrna_data, source, "See plot" ]
+		colnum=0
+		for i in column:
+			if i=="See plot":
+				index.write('<td class="column_' + str(colnum) + '">' + '<p><a href="html_plots/column'+ str(rownum) +'.html">See plot</a> </p>' + '</td>\n')
+				write_plot_html(gene_name, mrna_data, source, plot_dir, out_directory, rownum)
+			elif i==source:
+				index.write('<td class="column_' + str(colnum) + '">' + '<p><a href=' + str(source[0]) +'>GO to NCBI site</a> </p></td>\n')
+			elif len(mrna_data) > 1:
+				index.write('<td class="column_' + str(colnum) + '">')
+				for j in range(len(mrna_data)):
+					index.write(str(i[j]) + "  ")	
+				index.write('</td>\n')
+			else:
+				index.write('<td class="column_' + str(colnum) + '">' + str(i[0]) + '</td>'+ "\n")
+			colnum += 1
+		index.write('</tr></table>' + "\n")
 
-read_data_file.close()
+
+def do_analysis(data_directory, plot_directory, out_directory):
+	os.chdir(data_directory)
+	rownum=0
+	for input_file_data in glob.glob("*.txt"):
+		mrna_data, source, gene_name = extract_mrna(input_file_data)
+		write_html(mrna_data, source, gene_name, rownum, out_directory, plot_directory)
+		rownum += 1
+	
+
+script, data_directory, plot_directory, out_directory = argv
+
+#call function
+do_analysis(argv[1], argv[2], argv[3])
+ 
+
+	
+	
+
+
+
+
+
+
